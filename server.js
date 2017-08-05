791753e2827cefb9d537236ddaf9de6e398344ba
mc4tech <chavezmeguel@yahoo.com>
2017-08-05 11:10:56 -0400

    setupdepencies/data

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Reservation (DATA)
+// =============================================================
+var characters = [{
+  routeName: "1",
+  CustomerName: "Mike Jones",
+  phoneNumber: "404-251-5378",
+  customerEmail: "whowhomikeJones@email.com",
+  customerID: 1
+}, {
+  routeName: "2",
+  CustomerName: "Julio Jones",
+  phoneNumber: "404-243-7893",
+  customerEmail: "JJones@email.com",
+  customerID: 2
+},
+}];
+
