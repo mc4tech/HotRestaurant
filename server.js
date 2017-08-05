c891a6ccf0aac2d4dd25ef7be0eedb7f89850830
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-08-05 11:38:56 -0400

    the code is there...

--- server.js
+++ server.js
@@ -3,38 +3,66 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+// =============================================================
+
+
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 3000;
+// =============================================================
+
+
 
 // Sets up the Express app to handle data parsing
+// =============================================================
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// =============================================================
+
+
 
 // Reservation (DATA)
 // =============================================================
 var reservations = [{
   routeName: "1",
-  CustomerName: "Mike Jones",
+  customerName: "Mike Jones",
   phoneNumber: "404-251-5378",
   customerEmail: "whowhomikeJones@email.com",
   customerID: 1
 }, {
   routeName: "2",
-  CustomerName: "Julio Jones",
+  customerName: "Julio Jones",
   phoneNumber: "404-243-7893",
   customerEmail: "JJones@email.com",
   customerID: 2
-},
 }];
 // =============================================================
+
+
+
+// Create new reservation - takes in JSON formatted input
+// =============================================================
+app.post("/newreservation", function(req, res) {
+  var newReservation = req.body;
+  newReservation.routeName = newReservation.customerID;
+
+  console.log(newReservation);
+
+  reservations.push(newReservation);
+
+  res.json(newReservation);
+});
+// =============================================================
+
+
 
 // listening on PORT 3000
 // =============================================================
 app.listen((process.env.PORT || 3000), function() {
   console.log("App listening on PORT " + PORT);
 });
+//=============================================================
